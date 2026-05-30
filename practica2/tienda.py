6fcd548cf594d4ae5b48c5c10639cf86d061d195
YhoselinLisbethGomezRomero <20323744@clases.edu.sv>
2026-05-30 11:51:16 -0600

    Create tienda.py

---
+++ practica2/tienda.py
@@ -0,0 +1,14 @@
+print("**************************************")
+print("**       BIENVENIDO  A              **")
+print("**      LA TIENDITA DE LISBETH      **")
+print("**************************************")
+
+num_jugos = 2
+num_churros = 1
+num_paletas = 1.50
+
+producto_totales = num_jugos + num_churros + num_paletas
+
+print("Atualmente contamos con:")
+print("jugos:",num_jugos, "churros", num_churros, "paletas", num_paletas)
+print("En total tenemos", producto_totales, "producto")
