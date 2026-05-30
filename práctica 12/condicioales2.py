2c42d8ca985d6f0579ba8fb90649983fe95ace91
YhoselinLisbethGomezRomero <20323744@clases.edu.sv>
2026-05-30 15:40:25 -0600

    Create condicioales2.py

---
+++ práctica 12/condicioales2.py
@@ -0,0 +1,14 @@
+print("Escribe tu nombre:")
+nombre = input()
+
+#elif
+
+
+if nombre == "Lisbeth":
+    print("Saludo Lisbeth")
+elif nombre == "Yosselin":
+    print("Que bonito nombre yosselin")
+
+else:
+    print("Que extraño nombre")
+    
