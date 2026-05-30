e964ffa608e1ef0443a3b103913b120863acc26a
YhoselinLisbethGomezRomero <20323744@clases.edu.sv>
2026-05-30 12:01:08 -0600

    Create condicionales.py

---
+++ practica 9/condicionales.py
@@ -0,0 +1,13 @@
+#Lógico booleano bool
+#Verdadero / falso: True / false
+#dato = 5 < 5
+#< > ==
+
+print("Ingresa tu nombre")
+nombre = input()
+
+if nombre == "Lisbeth":
+    print("Bienvenido Lisbeth")
+else:
+    print("Que nombre tan extraño")
+    
