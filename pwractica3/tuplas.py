608c5a7cddee59c481b40b7bd11526284c313f33
YhoselinLisbethGomezRomero <20323744@clases.edu.sv>
2026-05-30 11:53:45 -0600

    Create tuplas.py

---
+++ pwractica3/tuplas.py
@@ -0,0 +1,12 @@
+#Tuplas my similares a las listas
+#No ´pueden modificarse
+#Se utilizan parentesis en lugar de []
+
+listas = ["Rodrigo", "Juan"] #Constantemente agregado información
+tupla = ("Rodrigo", "Juan")
+
+videojuego = []
+#Nombres, Genero, Años
+videojuego.append( ("Final Fantasy 7", "JRPG", 1997) )
+videojuego.append( ("Outer wilds", "Aventura", 2019) )
+print(videojuego)
